eef011160ba6b564ed87189c06a1d497c112a8c9
Aviv454 <Aviv454@gmail.com>
2017-11-14 16:33:49 +0200

    server can return a user

---
+++ client-side-chat/src/app/rooms-component/rooms-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RoomsComponentComponent } from './rooms-component.component';
+
+describe('RoomsComponentComponent', () => {
+  let component: RoomsComponentComponent;
+  let fixture: ComponentFixture<RoomsComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RoomsComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoomsComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client-side-chat/src/app/rooms-component/rooms-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-component',
+  templateUrl: './rooms-component.component.html',
+  styleUrls: ['./rooms-component.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RoomsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
